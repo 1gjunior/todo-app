2ef2ffc977bef705dbb1094cb1c12709a1d56fb3
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-07 16:40:38 -0300

    cleanup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario kart' },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todos = () => {
+  return (
+    <div className="todos collection">
+      <h1>h</h1>
+    </div>
+  );
+};
+
+export default Todos;
